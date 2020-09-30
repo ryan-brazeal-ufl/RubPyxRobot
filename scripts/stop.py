c09673a3f6feeac4017386530930a64a6b29dff2
Ryan Brazeal @ UFL <ryan.brazeal@ufl.edu>
2020-09-30 08:22:12 -0600

    Delete stop.py

--- scripts/stop.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-from driver import PWM
-
-# Initialise the PWM device using the default address
-pwm = PWM(0x40)
-pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
-pwm.softwareReset()
